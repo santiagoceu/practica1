3fae56c344d51e506206c3e232c9a37a7378312b
santiagoceu <santiago.vidal@usp.ceu.es>
2022-12-09 11:44:52 +0100

    Creado fichero Articulo pero no está listo

---
+++ src/dominio/Articulo.java
@@ -0,0 +1,13 @@
+package dominio
+
+import java.util.BigDecimal;
+import java.util.ArrayList;
+
+public class Articulo {
+	private String nombre;
+	private BigDecimal precio;
+	private ArrayList<String> caracter;
+
+	public Articulo(String n, String p) {
+
+
