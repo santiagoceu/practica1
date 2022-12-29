c55b124a28e3b4095b84c5e824e3e6082575c241
santiagoceu <santiagoceu@localhost.localdomain>
2022-12-29 23:51:55 +0100

    Annade comentarios a Articulo.java

--- src/dominio/Articulo.java
+++ src/dominio/Articulo.java
@@ -6,36 +6,36 @@ import java.util.Arrays;
 import java.io.Serializable;
 
 public class Articulo implements Serializable {
-	private String nombre;
-	private BigDecimal precio;
-	private ArrayList<String> tags;
-	public Articulo(String n, String p, String[] t) {
+	private String nombre;						// nombre del articulo
+	private BigDecimal precio;					// precio del articulo
+	private ArrayList<String> tags;					// informacion adicional del articulo, usado para facilitar la busqueda
+	public Articulo(String n, String p, String[] t) {		// Constructor de Articulo
 		this.nombre = n;
 		this.precio = new BigDecimal(p);
-		this.tags = new ArrayList<String>(Arrays.asList(t));
+		this.tags = new ArrayList<String>(Arrays.asList(t));	// Se toma un String array al crearse, aportado por la interfaz.
 	}
 
-	public String getNombre() {
+	public String getNombre() {					// get nombre de Articulo
 		return this.nombre;
 	}
 
-	public void setNombre(String n) {
+	public void setNombre(String n) {				// Sobreescribir nombre de Articulo
 		this.nombre = n;
 	}
 
-	public BigDecimal getPrecio() {
+	public BigDecimal getPrecio() {					// get precio de Articulo en BigDecimal
 		return this.precio;
 	}
 
-	public void setPrecio(String p) {
+	public void setPrecio(String p) {				// set precio, recibe un string que se convierte a BigDecimal
 		this.precio = new BigDecimal(p);
 	}
 
-	public ArrayList getTags() {
+	public ArrayList getTags() {					// Get tags del Articulo
 		return this.tags();
 	}
 
-	public void setTags(String[] t) {
+	public void setTags(String[] t) {				// Soobrescribe tags con nuevas tags.
 		this.tags = new ArrayList<String>(Arrays.asList(t));
 	}
 }
